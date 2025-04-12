33517465a2b7c464d374bd4f17cce35d1589d114
Skyfrei <tarkaklavio@gmail.com>
2025-04-12 18:36:43 +0200

    changes to compiler

--- compile.sh
+++ compile.sh
@@ -1,3 +1,13 @@
+if [ ! -d "SDL" ]; then
+   echo "SDL doesn't exist" 
+   git clone https://github.com/catchorg/Catch2.git
+fi
+
+if [ ! -d "Catch2" ]; then
+    echo "Catch2 doesn't exist"
+    git clone https://github.com/libsdl-org/SDL.git
+fi
+
 cd build/
 
 ninja
